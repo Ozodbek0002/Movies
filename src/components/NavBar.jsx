83063c975724e67280b695014e0c14bad63f7e23
murodjon29 <smurodjon983@gmail.com>
2025-11-22 02:18:14 +0500

    task bajarildi

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,51 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../css/NavBar.css";
+import Logo from "../assets/icons/logo.png";
+import { User } from "lucide-react";
 
 function NavBar() {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <Link to="/">Movie Explorer</Link>
+        <NavLink to="/">
+          <img className="logo" src={Logo} alt="Logo" />
+        </NavLink>
       </div>
+
       <div className="navbar-links">
-        <Link to="/" className="nav-link">
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+        >
           Home
-        </Link>
-        <Link to="/favorities" className="nav-link">
-          Favorities
-        </Link>
+        </NavLink>
+
+        <NavLink 
+          to="/contact" 
+          className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </NavLink>
+
+        <NavLink 
+          to="/about" 
+          className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+        >
+          About
+        </NavLink>
+
+        <NavLink 
+          to="/favorities" 
+          className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+        >
+          Favorites
+        </NavLink>
+        <NavLink to='/signup' className="nav-link signup-btn">
+          <User />
+        </NavLink>
       </div>
     </nav>
   );
 }
+
 export default NavBar;
